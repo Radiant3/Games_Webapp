63d3615d2a0547dd7a4e5ff00894671cca0e3697
RowanAlbertAlex <ralb0005@student.monash.edu>
2024-04-06 18:01:57 +1100

    small design changes to home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,7 +2,9 @@ export function Home(){
     return (
         <>
         <div class="black">
-        <h1>Welcome to the Game Menu</h1>
+        <h1>Welcome to Bored Gaming</h1>
+            <h2>Please select a game</h2>
+
         </div>
         </>
     )
