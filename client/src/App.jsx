63e0dca61ecaeff2788aa997ab2b68418503c5c0
rjag0003 <112871783+rjag0003@users.noreply.github.com>
2024-04-06 14:23:02 +1100

    Fixed the css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-// import './pages.css'
-// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -14,57 +10,17 @@ import { Page3 } from './pages/page3'
 import  Layout  from './pages/layout.jsx'
 
 
-// import {
-//     HasRouter, Routes, Route
-// } from "react-router-dom";
-
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
 export function App(){
   return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home/>}/>
-          <Route path="Game" element={<Game/>}/> 
-          <Route path="Page2" element={<Page2/>}/> 
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="Game" element={<Game/>}/> 
+        <Route path="Page2" element={<Page2/>}/> 
+      </Route>
+    </Routes>
   );
-  // <Router>
-  //     <Routes>
-  //       <Route path="/" element={<Home/>}/>
-  //     </Routes>
-  // </Router>
+
 }
 
 export default App;
--- client/src/pages/layout.jsx
+++ client/src/pages/layout.jsx
@@ -1,21 +1,30 @@
 import { Outlet, Link } from "react-router-dom";
-
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Homepage</Link>
-          </li>
-          <li>
-            <Link to="/Game">Tic-Tac-Toe</Link>
-          </li>
-          <li>
-            <Link to="/page2">Sudoku</Link>
-          </li>
-        </ul>
-      </nav>
+    <nav class="navbar bg-dark" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">ICON</a>
+        <div id="navbarNav">
+          <ul id="navbar-nav">
+
+            <li class="nav-item margin">
+              <a aria-current="page" href="#"><Link to="/">Cool Games</Link></a>
+            </li>
+
+            <li class="nav-item margin">
+              <a class="nav-link" href="#"><Link to="/Game">Tic-Tac-Toe</Link></a>
+            </li>
+
+            <li class="nav-item margin">
+              <a class="nav-link" href="#"><Link to="/page2">Sudoku</Link></a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+
 
       <Outlet />
     </>
